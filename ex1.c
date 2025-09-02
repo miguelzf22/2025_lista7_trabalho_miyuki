bf6fc866cbb62363f4fb691b0b549ac9442637bf
Enkdt <thalesendo@gmail.com>
2025-09-02 20:06:44 -0300

    ex1.c

--- ex1.c
+++ ex1.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
+int a[2];
+int b[2];
+int pf =0;
 
-int main(){
-	int a;
-	int b;
-	int a2 = 3; //Você pode alterar o placar modificando as variáveis a2 e b2.
-	int b2 = 4;
-	int pf;
-	
-	printf( "Ola, bem-vindo ao programa de apostas!\nQual e o placar do time A?\n ");
-	scanf("%d",&a);
-	printf("E o placar do time B?\n");
-	scanf("%d",&b);
-	printf("O placar e de %d x %d\n",a2,b2);
-	if(a>b&&a2>b2){
-		pf+=10;
-		printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
-	}
-	if(b>a&&b2>a2){
-		pf+=10;
-		printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
-	}
-	if(a2==b2&&a==b){
-		printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
-		pf+=10;
-	}
-	
-
-	
-	if(a==a2){
-		pf+=5;
-		printf("\nVocê ganhou 5 pontos por acertar o placar\n");
-	}
-	if(b==b2){
-		pf+=5;
-		printf("\nVocê ganhou 5 pontos por acertar o placar\n");
-	}
-	
-	printf("\nVocê fez %d pontos.",pf);
+void print(){
+	printf("Ola, bem-vindo ao programa de apostas!\nQual e o placar do time A e time B? (Digite n1 n2)\n");
+	scanf("%d %d",&a[0],&b[0]);
+	printf("Qual e o placar final do jogo? (Digite n1 n2)\n");
+	scanf("%d %d",&a[1],&b[1]);
 }
 
+void jogo(){
+ if(b[0]>a[0]&&b[1]>a[1]){
+		pf+=10;
+		printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
+	}
+	else if(a[0]>b[0]&&a[1]>b[1]){
+		pf+=10;
+	printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
+	}
+	else if(a[0]==b[0]&&a[1]==b[1]){
+		printf("\nVocê ganhou 10 pontos por acertar o resultado\n");
+		pf+=10;
+	}
+	
+	if(a[0]==a[1]){
+		pf+=5;
+		printf("\nVocê ganhou 5 pontos por acertar o placar\n");
+	}
+	if(b[0]==b[1]){
+		pf+=5;
+		printf("\nVocê ganhou 5 pontos por acertar o placar\n");
+	}
+}
 
+int main(){
+	print();
+	printf("O placar e de %d x %d\n",a[1],b[1]);
+   	jogo();
+	printf("\nVocê fez %d pontos.",pf);
+}
